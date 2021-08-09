f6b1229ea89355729f81b44f60cff87abaadcffe
Daniel <info@daniel-bird.co.uk>
2021-08-09 12:14:29 +0100

    Pass test

--- appointments/test/CustomerForm.test.js
+++ appointments/test/CustomerForm.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactTestUtils from 'react-dom/test-utils';
 import { createContainer } from './domManipulators';
 import { CustomerForm } from '../src/CustomerForm';
 
@@ -49,5 +50,18 @@ describe('CustomerForm', () => {
   it('assigns an ID that matches the label id to the first name field', () => {
     render(<CustomerForm />);
     expect(firstNameField().id).toEqual('firstName');
+  });
+
+  it('saves existing first name when submitted', async () => {
+    expect.hasAssertions();
+
+    render(
+      <CustomerForm
+        firstName="Ashley"
+        onSubmit={({ firstName }) => expect(firstName).toEqual('Ashley')}
+      />
+    );
+
+    await ReactTestUtils.Simulate.submit(form('customer'));
   });
 });
