28263bbd4d6f5cf3dc01ecf2a6feaf8149532118
Daniel <info@daniel-bird.co.uk>
2021-08-09 13:07:26 +0100

    Render form

--- appointments/src/index.js
+++ appointments/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { AppointmentsDayView } from './AppointmentsDayView';
-import { sampleAppointments } from './sampleData';
+import { CustomerForm } from './CustomerForm';
+// import { AppointmentsDayView } from './AppointmentsDayView';
+// import { sampleAppointments } from './sampleData';
+import(CustomerForm);
 
-ReactDOM.render(
-  <AppointmentsDayView appointments={sampleAppointments} />,
-  document.getElementById('root')
-);
+ReactDOM.render(<CustomerForm />, document.getElementById('root'));
